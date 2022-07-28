2087d94e73434cdc7e13df7cdf76f29301eccf9f
zhxxltx <3136713504@qq.com>
2022-07-28 22:57:27 +0800

    Initial commit

---
+++ src/main/java/com/zhx/InterfaceIdempotencyApplication.java
@@ -0,0 +1,13 @@
+package com.zhx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceIdempotencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterfaceIdempotencyApplication.class, args);
+    }
+
+}
